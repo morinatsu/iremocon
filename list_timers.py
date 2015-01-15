f600f21080f67a53b959ca00bf36e6b41edc4535
Natsuki Mori <morinatsu@gmail.com>
2015-01-15 20:24:29 +0900

    Improve answer message.

--- list_timers.py
+++ list_timers.py
@@ -7,6 +7,27 @@ List Registerd Timers
 import datetime
 from iremocon import IRemocon
 
+def reparse_time(seconds):
+    date_time = datetime.datetime(1970, 1, 1, 9, 0, 0) + \
+        datetime.timedelta(seconds=int(seconds))
+    return date_time.strftime('%Y/%m/%d %H:%M:%S')
+
 remocon = IRemocon('iremocon.yaml')
+
+# send command
 answer = remocon.SendCommand(b'*tl\r\n').decode('ascii')
 print('Recieved: ', repr(answer))
+
+# parse answer
+if answer.startswith('tl;ok;'):
+    head = answer.rstrip('\r\n').split(';')[0:2]
+    body = answer.rstrip('\r\n').split(';')[3:]
+    while len(body) > 0:
+        seq = body.pop(0)
+        code = remocon.inverted_code[body.pop(0)]
+        time = reparse_time(body.pop(0))
+        repeat = body.pop(0)
+        print('Seq: {seq}, Code: {code}, Time: {time}'.format(seq=seq,
+            code=code, time=time))
+else:
+    print('error')
--- sence.py
+++ sence.py
@@ -1,5 +1,5 @@
-#!/env/python
-#! -*- coding:utf-8 -*-
+#!/usr/bin/env python
+#! -*- coding: utf-8 -*-
 """
 Display Info of Sencers in i-remocon
 """
@@ -8,6 +8,18 @@ from iremocon import IRemocon
 
 
 remocon = IRemocon('iremocon.yaml')
+
+# send command
 answer = remocon.SendCommand(b'*se\r\n').decode('ascii')
 print('Recieved', answer)
 
+# parse answer
+if answer.startswith('se;ok;'):
+    illuminance = float(answer.rstrip('\r\n').split(';')[2])
+    humidity = float(answer.rstrip('\r\n').split(';')[3])
+    temperature = float(answer.rstrip('\r\n').split(';')[4])
+    print('illuminance: {illum:.0f}lx'.format(illum=illuminance))
+    print('humidity: {humidity:.0f}%'.format(humidity=humidity))
+    print('temperature: {temper:.0f}°C'.format(temper=temperature))
+else:
+    print('Error')
