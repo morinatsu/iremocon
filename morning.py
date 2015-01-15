91b70b3ada3e7b4e3ede4b9a6eb3e3648092979a
Natsuki Mori <morinatsu@gmail.com>
2015-01-15 20:30:18 +0900

    PEP8, add comment.

--- morning.py
+++ morning.py
@@ -8,17 +8,25 @@ import datetime
 import argparse
 from iremocon import IRemocon
 
+# Parse Argument
 parser = argparse.ArgumentParser(description='Turn On RoomLight.')
-parser.add_argument('datetime', type=str, help='time when turn on room light')
+parser.add_argument('datetime', type=str,
+                    help='date & time when turn on room light')
 args = parser.parse_args()
 
-alarm_time = datetime.datetime.strptime(args.datetime, '%Y/%m/%d %H:%M:%S')
-t = alarm_time - datetime.datetime(1970,1,1,9,0,0)
-command_string = '*tm;350;{seconds:.0f};0\r\n'.format(seconds=t.total_seconds())
 remocon = IRemocon('iremocon.yaml')
 
 # remocon code to turn on room light
 turn_on = remocon.code['6畳']['全灯']
+
+# make command_string
+alarm_time = datetime.datetime.strptime(args.datetime,
+                                        '%Y/%m/%d %H:%M:%S')
+t = alarm_time - datetime.datetime(1970, 1, 1, 9, 0, 0)
+command_string = \
+    '*tm;{REMOCON_CODE};{seconds:.0f};0\r\n'.format(
         REMOCON_CODE=turn_on, seconds=t.total_seconds())
+
+# send command and display result
 answer = remocon.SendCommand(command_string.encode('ascii'))
 print('Recieved', repr(answer))
--- now.py
+++ now.py
@@ -8,11 +8,16 @@ import datetime
 from iremocon import IRemocon
 
 remocon = IRemocon('iremocon.yaml')
+
+# send command
 answer = remocon.SendCommand(b'*tg\r\n').decode('ascii')
+
+# parse answer
 if answer.startswith('tg;ok;'):
     seconds = int(answer.rstrip('\r\n').rpartition(';')[2])
-    now = datetime.datetime(1970, 1, 1, 9, 0, 0) + datetime.timedelta(seconds=seconds)
+    now = datetime.datetime(1970, 1, 1, 9, 0, 0) + \
+        datetime.timedelta(seconds=seconds)
     print('Time of i-remocon: ', now)
 else:
-    print('error')
+    print('Error: ', repr(answer))
 
